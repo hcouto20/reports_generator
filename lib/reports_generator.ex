0e8e542038cc1c7055da5f6ee584d78950cf39fe
hcouto20 <helder@brassun.com.br>
2021-08-30 20:47:06 -0300

    Usando Enum.each com fn para mostrat conteúdo do arquivo

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -2,11 +2,9 @@ defmodule ReportsGenerator do
 
   def build(filename) do
     "reports/#{filename}"
-    |> File.stream!() #traz struct, não tem o conteudo do arquivo
-    |> handle_file("banana")
+    |> File.stream!()
+    |> Enum.each(fn elem -> IO.inspect(elem)
+  end)
   end
-
-  defp handle_file({:ok, file_content}, msg), do: file_content <> msg
-  defp handle_file({:error, _reason}, msg), do: "Error while opening file " <> msg
 
 end
