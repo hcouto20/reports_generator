5fad7cde71226b87fea65daf6dada736f8650108
hcouto20 <helder@brassun.com.br>
2021-08-30 20:40:36 -0300

    refactor function build, concated function

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -3,10 +3,10 @@ defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
     |> File.read()
-    |> handle_file()
+    |> handle_file("banana")
   end
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while opening file"
+  defp handle_file({:ok, file_content}, msg), do: file_content <> msg
+  defp handle_file({:error, _reason}, msg), do: "Error while opening file " <> msg
 
 end
